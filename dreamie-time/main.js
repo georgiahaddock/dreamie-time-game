565ad369d20a42af589789b52dc6aa62f370ff72
georgiahaddock <116069149+georgiahaddock@users.noreply.github.com>
2022-11-30 16:31:33 +0000

    button setup

--- dreamie-time/main.js
+++ dreamie-time/main.js
@@ -1,5 +1,3 @@
-//import Phaser, { Game } from "phaser";
-
 let dash;
 let treeStump;
 let mushroom1;
@@ -12,17 +10,19 @@ let isJumping;
 let dreamies;
 let collected = 0;
 let complete = false;
-// let meow_audio;
+let meow_audio;
+let button
 
 new Phaser.Game({
   type: Phaser.AUTO,
   width: 1350,
   height: 700,
+  pixelArt: true,
   physics: {
       default: 'arcade',
       arcade: {
-          gravity: { y: 300 },
-          debug: true
+          gravity: { y: 500 },
+          debug: false
       }
   },
   scene: { preload, create, update }
@@ -38,17 +38,21 @@ function preload () {
     this.load.image("bg7", "./assets/shroom-background/7.png");
 
     //load Dash the cat sprites
-    this.load.spritesheet('idle', './assets/cat-spritesheet.png', { frameWidth: 32, frameHeight: 32 })
-    this.load.spritesheet('run', './assets/cat-spritesheet.png', { frameWidth: 32, frameHeight: 32 })
-    this.load.spritesheet('jump', './assets/cat-spritesheet.png', { frameWidth: 32, frameHeight: 32 })
+    this.load.spritesheet('idle', './assets/cat-spritesheet.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('run', './assets/cat-spritesheet.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('jump', './assets/cat-spritesheet.png', { frameWidth: 32, frameHeight: 32 });
 
     //load floating log sprite
-    this.load.spritesheet('floating-log', './assets/floatingLog.png', { frameWidth: 814, frameHeight: 140 })
+    this.load.spritesheet('floating-log', './assets/floatingLog.png', { frameWidth: 814, frameHeight: 140 });
 
     //load dreamie sprite
-    this.load.spritesheet('dreamies', './assets/dreamies-spritesheet.png', { frameWidth: 716, frameHeight: 716 })
+    this.load.spritesheet('dreamies', './assets/dreamies-spritesheet.png', { frameWidth: 716, frameHeight: 716 });
 
-    // this.load.audio('meow url')
+    //load good boy button
+    this.load.spritesheet('button', './assets/button/button.png', { frameWidth: 1007 , frameHeight: 926 });
+
+    //audio files
+    this.load.audio('meow','./assets/audio/meow 1.mp3');
 }
 
 function create () {
@@ -60,28 +64,37 @@ function create () {
     this.add.image(400, 300, "bg3");
     this.add.image(420, 400, "bg2");
 
-    //initialise Dash the cat!
+    //initialise audio
+    meow_audio = this.sound.add('meow');
+
     dash = this.physics.add.sprite(0, 0, 'idle');
     dash.setScale(3);
     dash.setBounce(0.1);
     dash.setCollideWorldBounds(true);
+    dash.setSize(15, 15).setOffset(10,18);
+
+    button = this.physics.add.sprite(1100, 100, 'button');
+    button.setScale(0.2);
+    button.setCollideWorldBounds(true);
+    button.body.immovable = true;
+    button.body.allowGravity = false;
 
     //initialise invisible platforms that match the background
-    treeStump = this.physics.add.sprite(250, 470).setScale(5, 1);
-    treeStump.body.immovable = true;
-    treeStump.body.allowGravity = false;
+    // treeStump = this.physics.add.sprite(250, 470).setScale(5, 1);
+    // treeStump.body.immovable = true;
+    // treeStump.body.allowGravity = false;
 
-    mushroom1 = this.physics.add.sprite(850, 200).setScale(3, 1);
-    mushroom1.body.immovable = true;
-    mushroom1.body.allowGravity = false;
+    // mushroom1 = this.physics.add.sprite(850, 200).setScale(3, 1);
+    // mushroom1.body.immovable = true;
+    // mushroom1.body.allowGravity = false;
 
-    mushroom2 = this.physics.add.sprite(1000, 220).setScale(3, 1);
-    mushroom2.body.immovable = true;
-    mushroom2.body.allowGravity = false;
+    // mushroom2 = this.physics.add.sprite(1000, 220).setScale(3, 1);
+    // mushroom2.body.immovable = true;
+    // mushroom2.body.allowGravity = false;
 
-    mushroom3 = this.physics.add.sprite(150, 150).setScale(3, 1);
-    mushroom3.body.immovable = true;
-    mushroom3.body.allowGravity = false;
+    // mushroom3 = this.physics.add.sprite(150, 150).setScale(3, 1);
+    // mushroom3.body.immovable = true;
+    // mushroom3.body.allowGravity = false;
 
     // floatingLog = this.physics.add.sprite(575, 200, "floating-log").setScale(0.25);
     // floatingLog.body.immovable = true;
@@ -93,9 +106,9 @@ function create () {
       allowGravity: false
     })
 
-    for (var i = 0; i < 6; i++) {
+    for (var i = 0; i < 15; i++) {
       var x = Phaser.Math.RND.between(0, 1200);
-      var y = Phaser.Math.RND.between(0, 700);
+      var y = Phaser.Math.RND.between(150, 700);
 
       var logsAll = this.floatingLogs.create(x, y, 'floating-log');
       logsAll.setScale(0.16);
@@ -107,7 +120,7 @@ function create () {
     floatingLogs.children.iterate(log => {
       log.body.immovable = true;
     })
-    
+
 
     //set up the dreamies!!
     this.dreamies = this.physics.add.group({
@@ -120,27 +133,27 @@ function create () {
       var x = Phaser.Math.RND.between(0, 1200);
       var y = Phaser.Math.RND.between(0, 700);
 
-      var dreamiesAll = this.dreamies.create(x, y, 'dreamies');
-      dreamiesAll.setScale(0.13);
+      while((x>1000 && y<250)){
+        x = Phaser.Math.RND.between(0, 1200);
+        y = Phaser.Math.RND.between(0, 700);
+      }
+
+      var dreamiesAll = this.dreamies.create(x, y, 'dreamies').setScale(0.15);
       dreamiesAll.setCollideWorldBounds(true);
     }
 
     dreamies = this.dreamies;
 
     //how all the objects interact with each other
-    this.physics.add.collider(dash, treeStump);
-    this.physics.add.collider(dash, mushroom1);
-    this.physics.add.collider(dash, mushroom2);
-    this.physics.add.collider(dash, mushroom3);
     this.physics.add.collider(dash, this.floatingLogs);
     this.physics.add.collider(dash, this.dreamies, (player, dreamie)=>{dreamie.destroy();
       collected ++;
       console.log(collected);
       document.getElementById("collected").innerHTML = collected;
+      meow_audio.play();
     })
 
     cursors = this.input.keyboard.createCursorKeys();
-    //meow = this.sound.add('meow')
 
     //Dash the cat animations
     this.anims.create({
@@ -186,6 +199,14 @@ function create () {
       frames: this.anims.generateFrameNumbers('dreamies', {end: 2}),
       frameRate: 6
     })
+
+    //button animation
+    this.anims.create({
+      key: 'button',
+      repeat: -1,
+      frames: this.anims.generateFrameNumbers('button', {end: 3}),
+      frameRate: 8
+    })
 }
 
 function update () {
@@ -196,6 +217,8 @@ function update () {
     dreamies.children.iterate(dreamie => {
       dreamie.anims.play('dreamies-anim', true);
     })
+
+    button.anims.play('button', true);
 
 
     if (cursors.left.isDown) {
@@ -235,7 +258,5 @@ function update () {
     //     audio_feet.play()
     // }
 
-    // if(!isWalking && audio_feet.isPlaying) {
-    //     audio_feet.stop()
-    // }
+
 }
